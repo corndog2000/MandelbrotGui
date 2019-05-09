7bfc9c300289406334d15e2a76bc284b9a8bfe60
Joe Schroedl <joe.schroedl@outlook.com>
2019-05-09 13:55:42 -0400

    Added zoom code, zoom don't work yet

--- Mandelbrot.py
+++ Mandelbrot.py
@@ -3,6 +3,15 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPainter, QColor
 
+xMin = -3
+xMax = 3
+yMin = -3
+yMax = 3
+widthScale = 3
+heightScale = 3
+zoomLevel = 2
+zoomedIn = False
+
 class Mandelbrot(QWidget):
 
     def __init__(self):
@@ -15,45 +24,89 @@ class Mandelbrot(QWidget):
         self.setWindowTitle('Mandelbrot')
         self.show()
 
+        global widthScale
+        global heightScale
+
+        size = self.size()
+        widthScale = 6 / size.width()
+        heightScale = 6 / size.height()
+
     def frange(self, start, stop, step):
         i = start
         while i < stop:
             yield i
             i += step
 
-    def linearInter(self, value, low, high, newLow, newHigh):
+    def linearMap(self, value, low, high, newLow, newHigh):
         return ((value - low) / (high - low)) * (newHigh - newLow)
 
     def mousePressEvent(self, event):
-        x = event.x()
-        y = event.y()
-        
-        print(x)
-        print(y)
+        global xMin
+        global xMax
+        global yMin
+        global yMax
+        global widthScale
+        global heightScale
+        global zoomedIn
 
-    def paintEvent(self, e):
+        zoomedIn = True
+        
+        size = self.size()
+        windowWidth = size.width()
+        windowHeight = size.height()
+
+        xMouse = event.x()
+        yMouse = event.y()
+        
+        xMouse = self.linearMap(xMouse, -(windowWidth / 2), (windowWidth / 2), xMin, xMax)
+        yMouse = self.linearMap(yMouse, -(windowHeight / 2), (windowHeight / 2), yMin, yMax)
+
+        #Make temporary variables to store the new x/y min/max so they aren't changed while the algorithms are still working
+        xMinTemp = xMouse - ((xMax - xMin) / (zoomLevel * zoomLevel))
+        xMaxTemp = xMouse + ((xMax - xMin) / (zoomLevel * zoomLevel))
+        yMinTemp = yMouse - ((yMax - yMin) / (zoomLevel * zoomLevel))
+        yMaxTemp = yMouse + ((yMax - yMin) / (zoomLevel * zoomLevel))
+
+        xMin = xMinTemp
+        xMax = xMaxTemp
+        yMin = yMinTemp
+        yMax = yMaxTemp
+
+        #Update scale for the new zoomed in view
+        widthScale = widthScale / ((zoomLevel * zoomLevel) / 1.5)
+        heightScale = heightScale / ((zoomLevel * zoomLevel) / 1.5)
+
+        self.repaint()
+        print("Done zooming in.")
+        print("New xMin: ", xMin)
+        print("New xMax: ", xMax)
+        print("New yMin: ", yMin)
+        print("New yMax: ", yMax)
+        print("New widthScale: ", widthScale)
+        print("New heightScale: ", heightScale)
+
+        #print(x)
+        #print(y)
+
+    def paintEvent(self, event):
         qp = QPainter()
         qp.begin(self)
         #self.drawPoints(qp)
-        self.drawMandelbrot(qp, 3, 3)
+        #Run the draw program
+        self.drawMandelbrot(qp, xMin, xMax, yMin, yMax, widthScale, heightScale)
         qp.end()
 
-    #Not used
-    #def drawPoints(self, qp):
-    #    size = self.size()
-
-    #    for x in range(0, size.width() - 1):
-    #        for y in range(0, size.height() - 1):
-    #            qp.setPen(Qt.blue)
-    #            qp.drawPoint(x, y)
-
-    def drawMandelbrot(self, qp, xMinMax, yMinMax):
+    def drawMandelbrot(self, qp, xMin, xMax, yMin, yMax, widthScale, heightScale):
         #Variables
         size = self.size()
         maxIteration = 25
+        
+        if zoomedIn is False:
+            widthScale = 6 / size.width()
+            heightScale = 6 / size.height()
 
-        for w in self.frange(-xMinMax, xMinMax, 6 / size.width()):
-            for h in self.frange(-yMinMax, yMinMax, 6 / size.height()):
+        for w in self.frange(xMin, xMax, widthScale):
+            for h in self.frange(yMin, yMax, heightScale):
 
                 x = 0
                 y = 0
@@ -67,19 +120,19 @@ class Mandelbrot(QWidget):
                     
                 if iteration != maxIteration:
                     if iteration <= 8:
-                        qp.setPen(QColor(self.linearInter(iteration, 0, 8, 0, 255), 255, 255))  #Red is based on iteration
+                        qp.setPen(QColor(self.linearMap(iteration, 0, 8, 0, 255), 255, 255))  #Red is based on iteration
                         #qp.setPen(Qt.red)
                     elif iteration > 8 and iteration <= 16:
-                        qp.setPen(QColor(255, self.linearInter(iteration, 9, 16, 0, 255), 255))  #Green is based on iteration
+                        qp.setPen(QColor(255, self.linearMap(iteration, 9, 16, 0, 255), 255))  #Green is based on iteration
                         #qp.setPen(Qt.green)
                     else:
-                        qp.setPen(QColor(255, 255, self.linearInter(iteration, 17, 25, 0, 255)))  #Blue is based on iteration
+                        qp.setPen(QColor(255, 255, self.linearMap(iteration, 17, 25, 0, 255)))  #Blue is based on iteration
                         #qp.setPen(Qt.blue)
                 else:
                     qp.setPen(Qt.black)
 
-                newW = self.linearInter(w, -3.0, 3.0, 0, size.width() - 1)
-                newH = self.linearInter(h, -3.0, 3.0, 0, size.height() - 1)
+                newW = self.linearMap(w, xMin, xMax, 0, size.width() - 1)
+                newH = self.linearMap(h, yMin, yMax, 0, size.height() - 1)
                 qp.drawPoint(newW, newH)
 
 def main():
