7f31bf131f3482083c49e6338a3c7c5a0b8ca746
Joe Schroedl <joe.schroedl@outlook.com>
2019-05-24 19:26:24 -0400

    Removed debug print statements

--- Julia.py
+++ Julia.py
@@ -4,12 +4,10 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPainter, QColor
 from multiprocessing import Process
 
-xMin = -3
-xMax = 3
-yMin = -3
-yMax = 3
-#widthScale = 3
-#heightScale = 3
+xMin = -1000
+xMax = 1000
+yMin = -1000
+yMax = 1000
 zoomLevel = 4
 
 class Mandelbrot(QWidget):
@@ -38,25 +36,8 @@ class Mandelbrot(QWidget):
         qp = QPainter()
         qp.begin(self)
         #Run the drawMandelbrot program
-        #self.runMultiprocessing(qp)
         self.drawMandelbrot(qp, xMin, xMax, yMin, yMax)
         qp.end()
-    
-    def runMultiprocessing(self, qp):
-        numberOfThreads = 4
-
-        totalLength = abs(xMin) + abs(xMax)
-        pieceLength = totalLength / numberOfThreads
-
-        for i in range(numberOfThreads):
-            xMinNew = xMin + (pieceLength * i)
-            xMaxNew = xMin + (pieceLength * (i + 1))
-
-            print("Process ", i, " started.")
-            p = Process(target=self.drawMandelbrot, args=(qp, xMinNew, xMaxNew, yMin, yMax))
-            p.start()
-        
-        p.join()
     
     def mousePressEvent(self, event):
         global xMin
@@ -71,18 +52,8 @@ class Mandelbrot(QWidget):
         xMouse = event.x()
         yMouse = event.y()
 
-        #print("xMouse: ", xMouse)
-        #print("yMouse: ", yMouse)
-        #print("Before Map - xMin: ", xMin)
-        #print("Before Map - yMin: ", yMin)
-        #print("Before Map - xMax: ", xMax)
-        #print("Before Map - yMax: ", yMax)
-
         xMouse = self.linearMap(xMouse, 0, windowWidth, xMin, xMax)
         yMouse = self.linearMap(yMouse, 0, windowHeight, yMax, yMin)
-
-        #print("xMouse: ", xMouse)
-        #print("yMouse: ", yMouse)
 
         #Make temporary variables to store the new x/y min/max so they aren't changed while the algorithms are still working
         xMinTemp = xMouse - ((xMax - xMin) / (zoomLevel * zoomLevel))
@@ -103,16 +74,6 @@ class Mandelbrot(QWidget):
         heightScale = (yMax - yMin) / size.height()
 
         self.repaint()
-        #print("Done zooming in.")
-        #print("New xMin: ", xMin)
-        #print("New xMax: ", xMax)
-        #print("New yMin: ", yMin)
-        #print("New yMax: ", yMax)
-        #print("New widthScale: ", widthScale)
-        #print("New heightScale: ", heightScale)
-
-        #print(x)
-        #print(y)
 
     def drawMandelbrot(self, qp, xMin, xMax, yMin, yMax):
         #Variables
@@ -145,20 +106,6 @@ class Mandelbrot(QWidget):
                         y = I
 
                     iteration += 1
-                '''    
-                if iteration != maxIteration:
-                    if iteration <= 33:
-                        qp.setPen(QColor(self.linearMap(iteration, 0, 33, 0, 255), 255, 255))  #Red is based on iteration
-                        #qp.setPen(Qt.red)
-                    elif iteration > 33 and iteration <= 66:
-                        qp.setPen(QColor(255, self.linearMap(iteration, 33, 66, 0, 255), 255))  #Green is based on iteration
-                        #qp.setPen(Qt.green)
-                    else:
-                        qp.setPen(QColor(255, 255, self.linearMap(iteration, 67, maxIteration, 0, 255)))  #Blue is based on iteration
-                        #qp.setPen(Qt.blue)
-                else:
-                    qp.setPen(Qt.black)
-                '''
 
                 if iteration != maxIteration:
                     qp.setPen(QColor.fromHsv(iteration, 255, 255))
@@ -169,10 +116,8 @@ class Mandelbrot(QWidget):
                 newH = self.linearMap(h, yMin, yMax, size.height() - 1, 0)
                 qp.drawPoint(newW, newH)
 
-def main():
+
+if __name__ == '__main__':
     app = QApplication([])
     ex = Mandelbrot()
     app.exec_()
-
-if __name__ == '__main__':
-    main()
